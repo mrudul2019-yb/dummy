a122e6f8b71186c2b2a76972ed1e2fd75848b1db
mrudul2019 <mrudulmnair@gmail.com>
2021-06-02 15:33:39 +0530

    completed the extension

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { purifyLogs } from './purifyLog'
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -13,14 +14,16 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('dummy.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	// let disposable = vscode.commands.registerCommand('dummy.helloWorld', () => {
+	// 	// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Dummy!');
-	});
+	// 	// Display a message box to the user
+	// 	vscode.window.showInformationMessage('Hello World from Dummy!');
+	// });
+	let purify = vscode.commands.registerCommand('extension.purify', purifyLogs);
 
-	context.subscriptions.push(disposable);
+	// context.subscriptions.push(disposable);
+	context.subscriptions.push(purify);
 }
 
 // this method is called when your extension is deactivated
